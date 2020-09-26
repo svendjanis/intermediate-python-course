f65943fe6258db29117998a6865e8471f55084ff
Sven <sven.djanis@gmail.com>
2020-09-26 18:22:16 +0200

    chore: rolling dices, sum up values of those #2

--- dice_roller.py
+++ dice_roller.py
@@ -2,9 +2,17 @@ import random
 
 def main():
 
-  roll = random.randint(1, 6)
+  dice_rolls = 2
+  dice_sum = 0
 
-  print(f'You rolled a {roll}')
+  for i in range(0, dice_rolls):
+    roll = random.randint(1, 6)
+    print(f'You rolled a {roll}')
+    dice_sum += roll
+
+
+  print(f'You have rolled a total of {dice_sum}')
+
 
 if __name__== "__main__":
   main()
