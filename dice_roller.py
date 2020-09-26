b03fd3675214631ad854589316a4096fdbd7299e
Sven <sven.djanis@gmail.com>
2020-09-26 18:49:40 +0200

    chore: competition in rolling dices between batman and joker

--- dice_roller.py
+++ dice_roller.py
@@ -4,21 +4,19 @@ def main():
 
   dice_rolls = int(input('How many dice would you like to roll? '))
   dice_size = int(input('How many sides are the dice? '))
-  dice_sum = 0
+  batman_sum = 0
+  joker_sum = 0
 
   for i in range(0, dice_rolls):
     roll = random.randint(1, dice_size)
-    print(f'You rolled a {roll}')
-    dice_sum += roll
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
-    elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
+    if i < dice_rolls / 2:
+      name = 'Batman'
+      batman_sum += roll
     else:
-      print(f'You rolled a {roll}')
-
-
-  print(f'You have rolled a total of {dice_sum}')
+      name = 'Joker'
+      joker_sum+= roll
+    print(f'{name} rolled a {roll}')
+  print(f'Batman rolled {batman_sum}, Joker rolled {joker_sum}')
 
 
 if __name__== "__main__":
